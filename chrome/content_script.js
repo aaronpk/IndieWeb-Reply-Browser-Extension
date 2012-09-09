b984569c7012587ead7c209f8e13630f8f909116
Aaron Parecki <aaron@parecki.com>
2012-09-09 11:16:55 -0700

    Unbind so we don't double-fire the click event

--- chrome/content_script.js
+++ chrome/content_script.js
@@ -6,12 +6,12 @@
 
 function bindTwitter() {
     $("a.js-action-reply").each(function(i,e){
-      $(e).click(function(evt){
+      $(e).unbind("click").click(function(evt){
         var tweet = $(evt.target).parents(".tweet");
         var url = "https://twitter.com/" + $(tweet).data('screen-name') + "/status/" + $(tweet).attr('data-item-id');
 
         // TODO: THIS IS A HACK! see below.
-        var postURL = "http://pk2.dev/admin/?reply_to=" + encodeURI(url);
+        var postURL = "http://pk.dev/admin/?reply_to=" + encodeURI(url);
         window.open(postURL);
 
         // TODO: Need to either get the page URL from the extension preferences, or send an event to the extension
@@ -32,7 +32,7 @@ $("a[data-reply-to='']").each(function(i,e){
   $(e).click(function(evt){
     var post = $(evt.target).parents(".post-container");
     var url = "https://alpha.app.net/" + $(post).data('post-author-username') + "/post/" + $(post).attr('data-post-id');
-    var postURL = "http://pk2.dev/admin/?reply_to=" + encodeURI(url);
+    var postURL = "http://pk.dev/admin/?reply_to=" + encodeURI(url);
     window.open(postURL);
     return false;
   });
