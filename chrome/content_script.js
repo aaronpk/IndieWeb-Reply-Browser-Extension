c41333bfb93e89c74103fae59d48b3e95d735fbe
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-12-18 20:32:43 +0000

    Wrapped IndieWebReply in the module pattern, started abstracting UI from binding

--- chrome/content_script.js
+++ chrome/content_script.js
@@ -4,12 +4,10 @@
  * LICENSE file.
  */
 
-function bindTwitter() {
-    $("a.js-action-reply").each(function(i,e){
-      $(e).unbind("click").click(function(evt){
-        var tweet = $(evt.target).parents(".tweet");
-        var url = "https://twitter.com/" + $(tweet).data('screen-name') + "/status/" + $(tweet).attr('data-item-id');
-		
+IndieWebReplyModule = (function (){
+	// private
+	
+	function openNoteUI() {
 		var replace = {
 			url: encodeURI(url)
 		};
@@ -31,21 +29,51 @@ function bindTwitter() {
 			
 			window.open(postURL);
 		});
-		
+	
 		return false;
-      });
-    });
-}
-
-bindTwitter();
-setTimeout(bindTwitter, 5000);
-
-$("a[data-reply-to='']").each(function(i,e){
-  $(e).click(function(evt){
-    var post = $(evt.target).parents(".post-container");
-    var url = "https://alpha.app.net/" + $(post).data('post-author-username') + "/post/" + $(post).attr('data-post-id');
-    var postURL = "http://pk.dev/admin/?reply_to=" + encodeURI(url);
-    window.open(postURL);
-    return false;
-  });
-});
+	}
+	
+	function bindTwitter() {
+		$("a.js-action-reply").each(function(i,e){
+		  $(e).unbind("click").click(function(evt){
+			var tweet = $(evt.target).parents(".tweet");
+			var url = "https://twitter.com/" + $(tweet).data('screen-name') + "/status/" + $(tweet).attr('data-item-id');
+			
+			// TODO: call openNoteUI
+			
+			return false;
+		  });
+		});
+	}
+	
+	function bindAppDotNet() {
+		// Set up app.net reply URLs
+		$("a[data-reply-to='']").each(function(i,e){
+		  $(e).click(function(evt){
+			var post = $(evt.target).parents(".post-container");
+			var url = "https://alpha.app.net/" + $(post).data('post-author-username') + "/post/" + $(post).attr('data-post-id');
+			var postURL = "http://pk.dev/admin/?reply_to=" + encodeURI(url);
+			window.open(postURL);
+			return false;
+		  });
+		});
+	}
+	
+	function bindTwitterShareButtons() {
+		$('.twitter-share-button').each(function(i, e) {
+		
+		});
+	}
+	
+	// public
+	
+	return {
+		init: function () {
+			bindTwitter();
+			setTimeout(bindTwitter, 5000);
+			
+			bindAppDotNet();
+			bindTwitterShareButtons();
+		}
+	};
+}());
