098487020b5874f545d60b33852879753f39b62d
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-12-19 17:56:31 +0000

    Added kango framework

---
+++ samples/NotificationsDemo/src/common/main.js
@@ -0,0 +1,24 @@
+﻿var counter = 0;
+
+kango.ui.browserButton.addEventListener(kango.ui.browserButton.event.COMMAND, function(event) {
+	var notification = kango.ui.notifications.createNotification('Title', 'Notification number ' + (++counter), 'http://kangoextensions.com/images/logos/kango.png');
+	
+	var x = 0;
+	notification.addEventListener(notification.event.Click, function() {
+		if(x == 2) {
+			notification.close();
+		}
+		x++;
+		kango.console.log('Notification click');
+	});
+	
+	notification.addEventListener(notification.event.Show, function() {
+		kango.console.log('Notification show');
+	});
+	
+	notification.addEventListener(notification.event.Close, function() {
+		kango.console.log('Notification close');
+	});
+	
+	notification.show();
+});
